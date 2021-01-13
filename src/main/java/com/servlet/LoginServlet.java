912dd1f15eaa314033b5623f127d22c67ece6bfd
unknown <nituldeori63@gmail.com>
2021-01-13 20:36:33 +0530

    Added: Ability to extend the Servlet to accept a valid Name

--- src/main/java/com/servlet/LoginServlet.java
+++ src/main/java/com/servlet/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
@@ -22,11 +23,19 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         String user = request.getParameter("user");
+        String userregex = "^[A-Z]{1}[a-zA-Z\\s]{2,}$";
+
         String pwd = request.getParameter("pwd");
 
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if(userID.equals(user) && password.equals(pwd)) {
+        if(!Pattern.matches(userregex, user)){
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>UserName is not in correct format</font>");
+            rd.include(request, response);
+        }
+        else if(userID.equals(user) && password.equals(pwd)) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
