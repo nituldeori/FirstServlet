9529fb12a4c838daa6349df64d427f70735d4bf2
unknown <nituldeori63@gmail.com>
2021-01-13 20:42:07 +0530

    Added: Ability to extend the Servlet to accept a valid Password

--- src/main/java/com/servlet/LoginServlet.java
+++ src/main/java/com/servlet/LoginServlet.java
@@ -16,23 +16,29 @@ import java.util.regex.Pattern;
         urlPatterns = { "/LoginServlet" },
         initParams = {
                 @WebInitParam(name = "user", value = "Narayan"),
-                @WebInitParam(name = "password", value = "BridgeLabz")
+                @WebInitParam(name = "password", value = "BridgeLabz@123")
         }
 )
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         String user = request.getParameter("user");
-        String userregex = "^[A-Z]{1}[a-zA-Z\\s]{2,}$";
-
+        String userRegex = "^[A-Z]{1}[a-zA-Z\\s]{2,}$";
+        String pwdRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%&]).{8,}$";
         String pwd = request.getParameter("pwd");
 
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if(!Pattern.matches(userregex, user)){
+        if(!Pattern.matches(userRegex, user)){
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
             out.println("<font color=red>UserName is not in correct format</font>");
+            rd.include(request, response);
+        }
+        else if(!Pattern.matches(pwdRegex, pwd)){
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Password is not in correct format</font>");
             rd.include(request, response);
         }
         else if(userID.equals(user) && password.equals(pwd)) {
